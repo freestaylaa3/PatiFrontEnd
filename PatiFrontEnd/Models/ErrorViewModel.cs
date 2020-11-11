22e59dd53b806c78b9fe89e0e2de5245c7867b81
Furkan Okuk <furkanokuk7@hotmail.com>
2020-11-11 23:08:52 +0300

    Proje dosyası ekle.

---
+++ PatiFrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PatiFrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
